004322777680fd47dc792e9aa50bf124828e448f
BrendaCav14 <111449881+BrendaCav14@users.noreply.github.com>
2022-09-29 01:57:52 -0500

    version 1
    pantallas hechas:
    registro usuario
    inicio de sesion
    pagina de inicio
    chat personal
    chat privado
    pagina de grupo
    
    faltan paginas:
    agregar tareas
    crear grupos

---
+++ POI-V1/POI-PROYECTO/Grupo.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO FCFM-CHATS</title>
+
+    <link rel="stylesheet" href="Inicio.css">
+    <link rel="stylesheet" href="Grupo.css">
+
+    <?php include('Bootstrap.php') ?>
+
+</head>
+<body>
+
+<!-------------------------------------      NAVEGADOR     ------------------------------------------------>
+
+<?php include('NavBar.php') ?>
+   <!----------------------------------      SLIDE BAR     ------------------------------------------------>
+
+   <?php include('SlideBar.php') ?>
+
+<!-------------------------------------    GRUPO  ------------------------------------------------>
+
+<div class="apartado">
+
+
+<img src="IMG/grupo1.jpg" alt="grupo" style="max-height:180px; max-width:180px; border-radius:5px; margin-left:30px; margin-top:30px; margin-bottom:30px;">
+<br>
+<h3 style="font-weight:bold;">BDM</h3>
+<p style="text-align:center;  font-style: oblique;">Base de Datos Multimedia</p>
+<br>
+              
+<hr>
+
+<h6 style="font-weight:bold;">Canales</h6>
+
+                  <div class="options" style="font-size:15px;">
+                    <a class="dropdown-item" href="#">General</a>
+                    <a class="dropdown-item" href="#">Subgrupo 1</a>
+                    <a class="dropdown-item" href="#">Subgrupo 2</a>
+                    
+                   
+                  </div>
+
+</div>
+
+<div class="apartado2">
+
+<div class="menu-grupo">
+
+<img src="IMG/grupo1.jpg" alt="grupo" class="usuario-foto">
+<h6 style="font-weight:bold; margin-left:100px; margin-top:-30px;">General</h6>
+<h6 style="margin-left:200px; margin-top:-8px;">Publicaciones</h6>
+<h6 style="margin-left:330px; margin-top:-28px;">Archivos</h6>
+<h6 style="margin-left:430px; margin-top:-28px;">Tareas</h6>
+</div>
+
+
+<div class="flex2">
+  
+
+</div>
+
+
+</div>
+
+  <div class="form-group">
+    <input type="text" class="form-control" style="margin-top:-75px; margin-left:500px; width:700px;" placeholder="Escribe algo....">
+    <button type="button" class="btn btn-primary" style="margin-left:1210px; width:100px; margin-top:-70px;">>></button>
+  </div>
+
+  <script>
+
+  </script>
+</body>
+</html>
